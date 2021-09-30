4efa160f8fe42bd72096dda09fcbb701f169f88d
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2021-09-30 13:30:18 -0300

    atualizando proj

--- src/App.js
+++ src/App.js
@@ -1,64 +1,48 @@
 //import logo from './logo.svg';
-import './App.css';
-import HelloWord from './components/HelloWord';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import List from './components/List';
-import Evento from './components/Evento';
-import Form from './components/Form';
-import Condicional from './components/Condicional';
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Contato from './components/pages/Contato'
+import Empresa from './components/pages/Empresa'
+import Home from './components/pages/Home'
+
 
 function App() {
 
-  const name = 'Sérgio';
-  
-  const letraMaiscula = name.toUpperCase();
-  
-  const url = "https://via.placeholder.com/150";
-
-  function sum(a, b){
-    return a+b;
-  }
-
-  const nome = "Maria";
-
   return (
     
-    <div className="App">
-      
-      <h1>Hello, {letraMaiscula}</h1>
-      
-      <p>Soma {2+2}</p>
-      
-      <p>Soma {sum(9,1)}</p>
-      
-      <img src={url} alt="Imagem"/>
-      
-      <HelloWord/>
-      
-      <SayMyName nome="Sérgio"/>
-      
-      <SayMyName nome="Ana"/>
-      
-      <SayMyName nome={nome}/>
-
-      <Pessoa 
-        img="https://via.placeholder.com/300x300" 
-        nome="Rodrigo" 
-        idade="18" 
-        profissao="Programador"
-      />
-
-      <List/>
-
-      <Evento/>
-
-      <Form/>
-
-      <Condicional/>
-
-    </div>
-  
+    <Router>
+          
+        <Navbar />
+        
+        <Switch>
+          
+        <Route exact path="/">
+        
+        <Home />
+        
+        </Route>
+        
+        <Route path="/empresa">
+          
+        <Empresa />
+          
+        </Route>
+          
+        <Route path="/contato">
+          
+        <Contato />
+        
+        </Route>
+        
+        </Switch>
+        
+        <Footer />
+    
+    
+    </Router>
+    
   );
 }
 
